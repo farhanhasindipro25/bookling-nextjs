78626ba824cf8d74df9b9e2636ee6815925032bf
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-01-06 17:35:23 +0600

    Added hero section

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-
-import BaseNavbar from './components/BaseNavbar'
+import BaseNavbar from "./components/BaseNavbar";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
     <div>
-      <BaseNavbar/>
+      <BaseNavbar />
+      <HeroSection />
     </div>
-  )
+  );
 }
