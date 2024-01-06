4714ed53222ab378e8955b9f62a6b77a67235ad7
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-01-06 14:54:59 +0600

    Added navbar

---
+++ app/components/BaseNavbar.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function BaseNavbar() {
+  return (
+    <div className="bg-white py-4">
+      <div className="max-w-7xl mx-auto px-6">
+        <div className="flex items-center justify-between">
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/assets/logo/logo.jpg"
+              width={500}
+              height={500}
+              alt="logo"
+              className="w-10 h-full"
+            />
+            <p className="font-semibold text-blue-950">BOOKLING</p>
+          </Link>
+          <Link href="/login">
+            <button className="bg-white border border-blue-600 rounded-md text-sm font-semibold text-blue-600 px-4 py-3 hover:bg-blue-50">
+              LOGIN
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
