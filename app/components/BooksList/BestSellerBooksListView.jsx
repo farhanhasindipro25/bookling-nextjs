6f9fdc14fe45a0e190f6810ccfad17ee345bfdb8
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-01-08 14:00:40 +0600

    Fixed spacing issue in best sellers section

--- app/components/BooksList/BestSellerBooksListView.jsx
+++ app/components/BooksList/BestSellerBooksListView.jsx
@@ -19,7 +19,7 @@ export default function BestSellerBooksListView() {
   if (isError) return <BestSellerBookError />;
 
   return (
-    <div className="py-0 md:py-6 lg:py-24 space-y-4 md:space-y-12">
+    <div className="py-20 lg:py-24 space-y-4 md:space-y-12">
       <h2 className="text-2xl md:text-4xl lg:text-5xl font-semibold text-blue-950">
         BEST SELLERS
         <span className="text-xl md:text-3xl lg:text-4xl ml-4 text-blue-700">
